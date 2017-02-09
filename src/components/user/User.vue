8ed2c83d8c74ffd565e525414f0a837ea5d473c6
dianacamacho <dcam1901@yahoo.com>
2017-02-09 15:08:38 -0600

    dealing with changing route params

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,12 +2,30 @@
     <div>
       <h1>The User Page</h1>
       <hr>
+      <p>Loaded ID: {{ currentId }}</p>
       <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        id: this.$route.params.id
+      }
+    },
+    // watch: {
+    //   '$route'(to, from) {
+    //     // to and from are just routes
+    //     this.id = to.params.id;
+    //   }
+    // },
+    // could also have used a computed property to keep track of changing route params
+    computed: {
+      currentId() {
+        return this.id = this.$route.params.id;
+      }
+    },
     methods: {
       navigateToHome() {
         this.$router.push("/");
