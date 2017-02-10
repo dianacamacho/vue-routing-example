df2c47a1040099a6f7300e7526670758e72e1064
dianacamacho <dcam1901@yahoo.com>
2017-02-10 14:21:28 -0600

    confirm button logs value

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,9 +4,24 @@
       <p>Locale: {{ $route.query.locale }}</p>
       <p>Analytics: {{ $route.query.q }}</p>
       <hr>
-
+      <button @click="checkConfirmation" class="btn btn-primary">Confirm</button>
       <div style="height: 700px"></div>
       <p id="data">Some extra data</p>
     </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        confirmed: false
+      }
+    },
+    methods: {
+      checkConfirmation() {
+        this.confirmed = true;
+        console.log(this.confirmed);
+      }
+    }
+  }
+</script>
